1c14e05a09adfff6bbd731606ffe6e0cfc924807
Nicholas de Waal <nick.a.dewaal@gmail.com>
2021-06-02 16:33:37 +0900

    going home

--- src/components/navigation-overlay/NavigationOverlay.jsx
+++ src/components/navigation-overlay/NavigationOverlay.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
+import { gsap } from 'gsap'
 
 import {
   NavigationOverlayContainer,
@@ -14,6 +15,8 @@ const NavigationOverlay = ({
   activeSection,
 }) => {
   const navLinks = ['Home', 'About', 'Projects', 'Skills', 'Contact']
+  // REFS
+  const exitIcon = useRef(null)
 
   const handleClick = (index) => {
     window.fullpage_api.silentMoveTo(index + 1)
@@ -26,12 +29,20 @@ const NavigationOverlay = ({
     window.fullpage_api.setAllowScrolling(true)
   }
 
+  useEffect(() => {
+    gsap.from(exitIcon.current, {
+      autoAlpha: 0,
+      ease: 'none',
+      delay: 1,
+    })
+  }, [])
+
   return (
     <NavigationOverlayContainer
       showNavigation={showNavigation}
       activeSection={activeSection}
     >
-      <LargeIcon icon='exit' onClick={handleExit}>
+      <LargeIcon icon='exit' ref={exitIcon} onClick={handleExit}>
         <i className='fas fa-times'></i>
       </LargeIcon>
       <NavigationList id='#myMenu'>
